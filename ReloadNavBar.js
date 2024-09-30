05302097a7b0f11c3c579497b28e6a181f76e9d5
Thiciane Frata <thicianefernanda892@gmail.com>
2024-09-29 21:31:30 -0500

    :sparkles: feat: adiciona a função ReloadNavBar
    Implementa a função ReloadNavBar, permitindo uma barra de navegação reutilizável em todas as páginas.

---
+++ ReloadNavBar.js
@@ -0,0 +1,11 @@
+function ReloadNavBar() {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+        });
+}
+
+ReloadNavBar();
+
+//falta adicionar a classe active
