1a0b4cc2916279014a8dfd703428b0d3ef0f5620
Thiciane Frata <thicianefernanda892@gmail.com>
2024-11-16 22:15:00 -0500

    🐛 fix: correção das mascaras nas telas de cadastro de funcionário, cliente e fornecedor. Criação de mask cnpj

---
+++ JavaScript/mask.js
@@ -0,0 +1,29 @@
+var cpf = document.getElementById('cpf');
+var fone = document.getElementById('fone');
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    MascaraCpf();
+    MascaraFone();
+    MascaraCnpj();
+});
+
+function MascaraCpf() {
+    var cpfmascara = IMask(cpf,
+        {
+            mask: '000.000.000-00'
+        });
+}
+
+function MascaraFone() {
+    var fonemascara = IMask(fone,
+        {
+            mask: '(00) 00000-0000'
+        });
+}
+
+function MascaraCnpj() {
+    var cnpjMask = IMask(cnpj, {
+        mask: '00.000.000/0000-00'
+    });
+}
